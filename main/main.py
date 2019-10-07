1b0e2debc2d6446dda397a1b30ba36f614bd3717
i130233 <40578252+i130233@users.noreply.github.com>
2019-10-08 00:45:48 +0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,8 +1,32 @@
-from machine import Pin
-from time import sleep
+ from ota_update.main.ota_updater import OTAUpdater
+ from machine import Pin
+ from time import sleep
 
-led = Pin(2, Pin.OUT)
+ led = Pin(2, Pin.OUT)
+  
+ def download_and_install_update_if_available():
+     o = OTAUpdater('url-to-your-github-project')
+     o.download_and_install_update_if_available('wifi-ssid', 'wifi-password')
 
-while True:
-  led.value(not led.value())
-  sleep(0.1)
+
+ def start():
+     # your custom code goes here. Something like this: ...
+     # from main.x import YourProject
+     # project = YourProject()
+     # ...
+     #while True:
+     led.value(not led.value())
+     sleep(0.1)
+
+ def boot():
+     download_and_install_update_if_available()
+     start()
+
+
+ boot()
+
+
+
+
+
+
