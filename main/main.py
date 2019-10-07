9a6c799c66d60b849e909bac459c7f50443dcab9
i130233 <40578252+i130233@users.noreply.github.com>
2019-10-08 01:23:19 +0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,29 +1,27 @@
- from ota_update.main.ota_updater import OTAUpdater
- from machine import Pin
- from time import sleep
+from ota_update.main.ota_updater import OTAUpdater
+from machine import Pin
+from time import sleep
 
- led = Pin(2, Pin.OUT)
+led = Pin(2, Pin.OUT)
   
- def download_and_install_update_if_available():
-     o = OTAUpdater('https://github.com/i130233/blink-OTA.git')
-     o.download_and_install_update_if_available('Shani', 'tinku123')
+def download_and_install_update_if_available():
+ o = OTAUpdater('https://github.com/i130233/blink-OTA.git')
+ o.download_and_install_update_if_available('Shani', 'tinku123')
 
+def start():
+ # your custom code goes here. Something like this: ...
+ # from main.x import YourProject
+ # project = YourProject()
+ # ...
+ #while True:
+ led.value(not led.value())
+ sleep(0.1)
 
- def start():
-     # your custom code goes here. Something like this: ...
-     # from main.x import YourProject
-     # project = YourProject()
-     # ...
-     #while True:
-     led.value(not led.value())
-     sleep(0.1)
-
- def boot():
-     download_and_install_update_if_available()
-     start()
-
-
- boot()
+def boot():
+ download_and_install_update_if_available()
+ start()
+ 
+boot()
 
 
 
